7f037fc948745eb1c78552442f3c5ea58630ba37
mannveer <imanveer.00017@gmail.com>
2024-10-12 13:22:11 +0530

    Update emailTemplates.js

--- src/services/email/emailTemplates.js
+++ src/services/email/emailTemplates.js
@@ -1,14 +1,14 @@
 import ejs from 'ejs';
 import path from 'path';
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
 class EmailTemplates {
     async getTemplate(templateName,data) {
       // Fetch the template from a secure storage location (e.g., Azure Blob Storage, AWS S3)
       // For demonstration, returning a simple placeholder template
       // return `<html><body><h1>${templateName}</h1></body></html>`;
-      const __dirname = path.dirname(fileURLToPath(import.meta.url));
-      const templatePath = path.join(__dirname, '../../views', `${templateName}.html`);
+      // const __dirname = path.dirname(fileURLToPath(import.meta.url));
+      const templatePath = path.join(process.cwd(), './src/views', `${templateName}.html`);
       return await ejs.renderFile(templatePath, data);
     
     }
